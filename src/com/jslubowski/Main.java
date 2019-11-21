df10ba1d2190265d8181354a0ff40633d55b53e2
Jakub Ślubowski <jakubslubowski@gmail.com>
2019-11-21 17:27:28 +0100

    v0.21 Part of the algorithm implemented

--- src/com/jslubowski/Main.java
+++ src/com/jslubowski/Main.java
@@ -1,13 +1,13 @@
 package com.jslubowski;
 
-import com.jslubowski.dip.Skeletonization;
-import com.jslubowski.dip.SmoothImage;
+import com.jslubowski.model.ParkingSpace;
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.imgproc.Moments;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -15,68 +15,42 @@ public class Main {
 
     public static void main(String[] args) {
 
-	    System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // library load
+	    System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // library load, needed for OpenCV
 
+        // TODO Implement current path instead of hard-coded
         String projectFilePath = "D:\\wszystko\\studia\\semestr 7\\praca inzynierska\\ParkingLotMonitoring\\src\\data\\";
-        String filename = "example_lot4"; // name of the file to work on
-        int kernelSize = 3;
-
-        Mat sourceImage = new Mat(); // sourceImage variable initialization
+        String filename = "example_lot2"; // name of the picture to work on
 
 
-        // Fourth algorithm
-            {
-                // 1. Load an image
-                sourceImage = Imgcodecs.imread(projectFilePath + filename + ".jpg");
-                // 2. Crop
-                Rect rectCrop = new Rect(new Point(368, 440), new Point(668, 516));
-                sourceImage = new Mat(sourceImage, rectCrop);
-                Imgcodecs.imwrite(projectFilePath + filename + "_cropped.jpg", sourceImage);
-                // 2. Noise reduction through gaussian blur
-                Imgproc.GaussianBlur(sourceImage, sourceImage,
-                     new Size(3, 3), 0, 0, Core.BORDER_DEFAULT);
-//                Imgcodecs.imwrite(projectFilePath + filename + "_blurred.jpg", sourceImage);
-                // 3. Canny Edge detection
-                Mat edges = new Mat();
-                Imgproc.Canny(sourceImage, edges, 230, 300, kernelSize, true);
-//                Imgcodecs.imwrite(projectFilePath + filename + "_canny.jpg", edges);
-                // 4. Initialization of a kernel
-                Mat E = Mat.ones(kernelSize, kernelSize, 1);
-//            System.out.println(E.dump());
+        //Main method steps
+        // 1. Load an image
+        Mat sourceImage = Imgcodecs.imread(projectFilePath + filename + ".jpg");
 
+        // TODO Implement reading from the file (point 2 and 3)
+        // 2. Initiate ParkingSpaces points on an image
+        ParkingSpace P1 = new ParkingSpace(425, 460, 480, 535, sourceImage, "P1");
+        ParkingSpace P2 = new ParkingSpace(480, 440, 545, 535, sourceImage, "P2");
+        ParkingSpace P3 = new ParkingSpace(545, 440, 595, 535, sourceImage, "P3");
+        ParkingSpace P4 = new ParkingSpace(600, 440, 660, 535, sourceImage, "P4");
+        ParkingSpace P5 = new ParkingSpace(275, 550, 360, 665, sourceImage, "P5");
+        ParkingSpace P6 = new ParkingSpace(365, 550, 450, 665, sourceImage, "P6");
+        ParkingSpace P7 = new ParkingSpace(450, 550, 540, 665, sourceImage, "P7");
+        ParkingSpace P8 = new ParkingSpace(540, 550, 720, 645, sourceImage, "P8");
+        ParkingSpace P9 = new ParkingSpace(620, 550, 720, 645, sourceImage, "P9");
+        ParkingSpace P10 = new ParkingSpace(760, 430, 825, 520, sourceImage, "P10");
+        ParkingSpace P11 = new ParkingSpace(840, 440, 880, 520, sourceImage, "P11");
+        ParkingSpace P12 = new ParkingSpace(885, 430, 945, 510, sourceImage, "P12");
+        ParkingSpace P13 = new ParkingSpace(945, 430, 1010, 510, sourceImage, "P13");
 
-                // 5. Dilation
-                Imgproc.dilate(edges, edges, E, new Point(-1, -1), 4);
-//                Imgcodecs.imwrite(projectFilePath + filename + "_dilated.jpg", edges);
-//                // 6. Erosion
-//                Imgproc.erode(edges, edges, E, new Point(-1, -1), 1);
-//                Imgcodecs.imwrite(projectFilePath + filename + "_eroded.jpg", edges);
+        P13.saveProcessedImage(projectFilePath, filename, "cropped");
 
+        // 3. Create a list of parking spaces (list because of future purposes)
+        List<ParkingSpace> parkingSpaces = new ArrayList<>(Arrays.asList(P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13));
 
-                // 5. Find and draw contours
-                List<MatOfPoint> contours = new ArrayList<>();
-                Mat hierarchy = new Mat();
-                Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-                Mat drawing = Mat.zeros(sourceImage.size(), CvType.CV_8UC3);
-                Scalar color = new Scalar(0, 255, 0);
-                for (int i = 0; i < contours.size(); i++) {
-                    if(Imgproc.contourArea(contours.get(i)) > 2) {
-                        Imgproc.drawContours(drawing, contours, i, color, 2, Imgproc.LINE_4, hierarchy, 0, new Point());
-                    }
-                }
-//                Imgcodecs.imwrite(projectFilePath + filename + "_contours.jpg", drawing);
-                // 7. Find and draw rectangles
-                Mat rectangles = Mat.zeros(sourceImage.size(), CvType.CV_8UC3);
-                for(MatOfPoint c: contours) {
-                    Rect rect = Imgproc.boundingRect(c);
-                    if(rect.area() > 500) {
-                        Imgproc.rectangle(rectangles, rect, color, 1);
-                    }
-                }
-                Imgcodecs.imwrite(projectFilePath + filename + "_rectangles.jpg", rectangles);
+        // 4. Run analysis
+        P3.isOccupied();
 
 
 
-            }
         }
     }
--- src/com/jslubowski/model/ParkingSpace.java
+++ src/com/jslubowski/model/ParkingSpace.java
@@ -1,49 +1,134 @@
 package com.jslubowski.model;
 
-import org.opencv.core.Mat;
+import org.opencv.core.*;
+import org.opencv.core.Point;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParkingSpace {
 
     // == fields ==
-
-    private ParkingSpaceCorner corner;
-    private int width;
-    private int length;
+    private String name;
+    private Point cornerTopLeft;
+    private Point cornerBottomRight;
     private Mat image;
+    private Mat imageProcessed;
 
     // == constructor ==
 
-    public ParkingSpace(int x, int y, int width, int length, Mat image) {
-        this.image = image;
-        this.corner = new ParkingSpaceCorner(x, y);
-        this.width = width;
-        this.length = length;
+    public ParkingSpace(int x1, int y1, int x2, int y2, Mat image, String name) {
+        this.cornerTopLeft = new Point(x1, y1);
+        this.cornerBottomRight = new Point(x2, y2);
+        this.name = name;
+        // crop out the parking spot from the image
+        Rect rectCrop = new Rect(cornerTopLeft, cornerBottomRight);
+        this.image = new Mat(image, rectCrop);
+        this.imageProcessed = this.image;
     }
+
+    // == methods ==
+    public void saveProcessedImage(String projectFilePath, String filename, String state){
+        Imgcodecs.imwrite(projectFilePath + filename + "_" + name + "_" + state + ".jpg", imageProcessed);
+    }
+
+    // this is the main algorithm for detecting empty parking space
+    public void isOccupied(){
+        Mat edges = new Mat();
+        int kernelSize = 3;
+        Mat morphologyKernel = Mat.ones(3, 3, 1);
+
+        // ONLY FOR DEBUGGING - DELETE LATER
+        String projectFilePath = "D:\\wszystko\\studia\\semestr 7\\praca inzynierska\\ParkingLotMonitoring\\src\\data\\";
+        String filename = "example_lot2"; // name of the picture to work on
+        //DELETE
+
+        // Algorithm steps
+        // 1. Gaussian blur
+        Imgproc.GaussianBlur(image, imageProcessed, new Size(3, 3), 0, 0, Core.BORDER_DEFAULT);
+        saveProcessedImage(projectFilePath, filename, "blurred");
+
+        // 2. Canny Edge detection
+        Imgproc.Canny(imageProcessed, edges, 230, 300, kernelSize, false);
+        this.imageProcessed = edges;
+        saveProcessedImage(projectFilePath, filename, "canny");
+
+        // 4. Dilation
+        Imgproc.dilate(imageProcessed, imageProcessed, morphologyKernel, new Point(-1, -1), 3);
+        saveProcessedImage(projectFilePath, filename, "dilated");
+
+        // 5. Erosion
+        Imgproc.erode(imageProcessed, imageProcessed, morphologyKernel, new Point(-1, -1), 1);
+        saveProcessedImage(projectFilePath, filename, "eroded");
+
+        // 6. Find and draw rectangles around contours
+        findAndDrawRectangles();
+        saveProcessedImage(projectFilePath, filename, "rectangles");
+
+        // 7. Are conditions met?
+        // TODO implement it completely
+
+
+
+    }
+
+        // this is a method to call if you want to draw your rectangles
+        private List<Rect> findAndDrawRectangles(){
+            List<MatOfPoint> contours = findAndDrawContours();
+            List<Rect> retContours = new ArrayList<>();
+            Mat rectangles = Mat.zeros(imageProcessed.size(), CvType.CV_8UC3);
+            for(MatOfPoint c: contours) {
+                    Rect rect = Imgproc.boundingRect(c);
+                    if(rect.area() > 500) {
+                        Imgproc.rectangle(rectangles, rect, new Scalar(0, 255, 0), 1);
+                        retContours.add(rect);
+                    }
+                }
+            this.imageProcessed = rectangles;
+            return retContours;
+        }
+
+
+        private List<MatOfPoint> findAndDrawContours(){
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(imageProcessed, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            Mat drawing = Mat.zeros(imageProcessed.size(), CvType.CV_8UC3);
+            Scalar color = new Scalar(0, 255, 0);
+            for (int i = 0; i < contours.size(); i++) {
+                if(Imgproc.contourArea(contours.get(i)) > 2) {
+                    Imgproc.drawContours(drawing, contours, i, color, 2, Imgproc.LINE_4, hierarchy, 0, new Point());
+                }
+            }
+            this.imageProcessed = drawing;
+            return contours;
+        }
+
+
+
+
+
+
+
 
     // == getters and setters ==
-
-    public ParkingSpaceCorner getCorner() {
-        return corner;
+    public Point getCornerTopLeft() {
+        return cornerTopLeft;
     }
 
-    public void setCorner(ParkingSpaceCorner corner) {
-        this.corner = corner;
+    public void setCornerTopLeft(Point cornerTopLeft) {
+        this.cornerTopLeft = cornerTopLeft;
     }
 
-    public int getWidth() {
-        return width;
+    public Point getCornerBottomRight() {
+        return cornerBottomRight;
     }
 
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
+    public void setCornerBottomRight(Point cornerBottomRight) {
+        this.cornerBottomRight = cornerBottomRight;
     }
 
     public Mat getImage() {
@@ -52,5 +137,21 @@ public class ParkingSpace {
 
     public void setImage(Mat image) {
         this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Mat getImageProcessed() {
+        return imageProcessed;
+    }
+
+    public void setImageProcessed(Mat imageProcessed) {
+        this.imageProcessed = imageProcessed;
     }
 }
